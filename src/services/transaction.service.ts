8065a94538ed4b24475b98e3ce68981365ece6db
Istiak Mahmud <istiakmahmud0@gmail.com>
2024-05-24 13:14:56 +0600

    feat: self taansaction list has shown

--- src/services/transaction.service.ts
+++ src/services/transaction.service.ts
@@ -13,7 +13,7 @@ export const transactionService = {
       queryFn: async () => {
         // const client = await getApiClient(accessToken)
         const client = await getApiClient();
-        const res = await client.getAllTransactions({ user: "hasib" });
+        const res = await client.getAllTransactions();
         return res.data.data?.transactions;
       },
       //   enabled: !!user && user.role === "admin",
