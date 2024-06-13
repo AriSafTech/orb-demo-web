d1d79b9e4bc940ad2ade51fe901c9b56f3759ff9
istiakmahmud0 <71057614+istiakmahmud0@users.noreply.github.com>
2024-06-13 11:30:00 +0600

    feat: fix the skeleton width

--- src/components/custom/UserSkeleton.tsx
+++ src/components/custom/UserSkeleton.tsx
@@ -5,8 +5,8 @@ const UserSkeleton = () => {
   return (
     <div className="flex flex-col space-y-3">
       <div className="space-y-2">
+        <Skeleton className="h-4 w-[150px]" />
         <Skeleton className="h-4 w-[250px]" />
-        <Skeleton className="h-4 w-[200px]" />
       </div>
     </div>
   );
